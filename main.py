5b1d756e754334d3c70c6051c49e9977ad05f663
tudor127 <tudor.ganeanu@yahoo.com>
2020-04-02 16:45:55 +0300

    finished app

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+import datetime as dt
 from datetime import datetime
 import logging
 import os
@@ -32,29 +19,45 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    # session.pop('test')
-
     if 'email' in session:
         # Create a Cloud Datastore client.
         datastore_client = datastore.Client()
-        query = datastore_client.query(kind='users')
-        query.add_filter('email', '=', session['email'])
-        username = list(query.fetch())[0]['username']
-
         posts_query = datastore_client.query(kind='posts')
+        posts_query.order = ['-date_added']
         posts = list(posts_query.fetch())
-
-        return render_template('homepage.html',username=username,posts=posts)
+        posts1 = []
+        for post in posts:
+            post1 = dict()
+            post1['author_name'] = post['author_name']
+            fmt = '%Y-%m-%d %H:%M:%S'
+            post1['date_added'] = dt.datetime.strptime(post['date_added'].strftime(fmt),fmt)
+            post1['image_url'] = post['image_url']
+            post1['description'] = post['description']
+            post1['image_name'] = post['image_name']
+            labels = []
+            for label in json.loads(post['labels']):
+                for value in label.values():
+                    labels.append(value)
+            post1['labels'] = labels
+            posts1.append(post1)
+        return render_template('homepage.html',username=session['username'],posts=posts1)
     else:
         return render_template('login.html')
 
+@app.route('/newpost')
+def newpost():
+    if 'email' in session:
+        return render_template('new_post.html',username=session['username'])
+    else:
+        return render_template('login.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     email = request.form['email']
-    password = request.form['password']
+    username = request.form['displayName']
 
     session['email'] = email
+    session['username'] = username
 
     # Redirect to the home page.
     return redirect('/')
@@ -98,6 +101,10 @@ def addpost():
     description = request.form['description']
 
     photo = request.files['file']
+
+    author_email = session['email']
+
+    author_name = session['username']
 
     # Create a Cloud Storage client.
     storage_client = storage.Client()
@@ -154,6 +161,8 @@ def addpost():
     # Construct the new entity using the key. Set dictionary values for entity
     entity = datastore.Entity(key)
     entity['image_name'] = name
+    entity['author_email'] = author_email
+    entity['author_name'] = author_name
     entity['image_url'] = blob.public_url
     entity['description'] = description
     entity['labels'] = labels
@@ -170,77 +179,6 @@ def logout():
     session.clear();
     # Redirect to the home page.
     return redirect('/')
-
-
-@app.route('/upload_photo', methods=['GET', 'POST'])
-def upload_photo():
-    photo = request.files['file']
-
-    # Create a Cloud Storage client.
-    storage_client = storage.Client()
-
-    # Get the bucket that the file will be uploaded to.
-    bucket = storage_client.get_bucket(CLOUD_STORAGE_BUCKET)
-
-    # Create a new blob and upload the file's content.
-    blob = bucket.blob(photo.filename)
-    blob.upload_from_string(
-            photo.read(), content_type=photo.content_type)
-
-    # Make the blob publicly viewable.
-    blob.make_public()
-
-    # Create a Cloud Vision client.
-    vision_client = vision.ImageAnnotatorClient()
-
-    # Use the Cloud Vision client to detect a face for our image.
-    source_uri = 'gs://{}/{}'.format(CLOUD_STORAGE_BUCKET, blob.name)
-    image = vision.types.Image(
-        source=vision.types.ImageSource(gcs_image_uri=source_uri))
-    faces = vision_client.face_detection(image).face_annotations
-
-    # If a face is detected, save to Datastore the likelihood that the face
-    # displays 'joy,' as determined by Google's Machine Learning algorithm.
-    if len(faces) > 0:
-        face = faces[0]
-
-        # Convert the likelihood string.
-        likelihoods = [
-            'Unknown', 'Very Unlikely', 'Unlikely', 'Possible', 'Likely',
-            'Very Likely']
-        face_joy = likelihoods[face.joy_likelihood]
-    else:
-        face_joy = 'Unknown'
-
-    # Create a Cloud Datastore client.
-    datastore_client = datastore.Client()
-
-    # Fetch the current date / time.
-    current_datetime = datetime.now()
-
-    # The kind for the new entity.
-    kind = 'Faces'
-
-    # The name/ID for the new entity.
-    name = blob.name
-
-    # Create the Cloud Datastore key for the new entity.
-    key = datastore_client.key(kind, name)
-
-    # Construct the new entity using the key. Set dictionary values for entity
-    # keys blob_name, storage_public_url, timestamp, and joy.
-    entity = datastore.Entity(key)
-    entity['blob_name'] = blob.name
-    entity['image_public_url'] = blob.public_url
-    entity['timestamp'] = current_datetime
-    entity['joy'] = face_joy
-
-    # Save the new entity to Datastore.
-    datastore_client.put(entity)
-
-    # Redirect to the home page.
-    return redirect('/')
-
 
 @app.errorhandler(500)
 def server_error(e):
